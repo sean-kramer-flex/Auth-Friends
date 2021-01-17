e8ba1210c4e4b7de8a0953b51dd282c5292f80d5
Sean Kramer <seandominickramer@gmail.com>
2021-01-17 18:01:23 -0500

    Add function to store token to localstorage

--- friends/src/Components/LoginForm.js
+++ friends/src/Components/LoginForm.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, { useState } from 'react';
+import axios from 'axios'
+import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 
 const LoginForm = (props) => {
+
+  const initialState = {
+    username: '',
+    password: ''
+  }
+const [credentials, setCredentials] = useState(initialState)
+
+const onChangeHandler = (e) => {
+  setCredentials({...credentials, [e.target.name]: e.target.value})
+}
+
+const onSubmitHandler = (e) => {
+  e.preventDefault()
+  console.log('submitted');
+  axios.post('http://localhost:5000/api/login', credentials)
+  .then(res => {
+    localStorage.setItem('token', res.data.payload)
+    props.history.push('/')
+  })
+  
+  .catch(err => console.log(err))
+}
+
+
+
   return (
-    <Form inline>
+    <Container className="themed-container">
+    <Form onSubmit={onSubmitHandler}>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <Label for="username" className="mr-sm-2">Username</Label>
-        <Input type="text" name="username" id="userName" placeholder="enter name" />
+        <Input type="text" name="username" id="userName" placeholder="enter name" onChange={onChangeHandler}/>
       </FormGroup>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <Label for="loginPassword" className="mr-sm-2">Password</Label>
-        <Input type="password" name="password" id="loginPassword" placeholder="enter password" />
+        <Input type="password" name="password" id="loginPassword" placeholder="enter password" onChange={onChangeHandler}/>
       </FormGroup>
       <Button>Submit</Button>
     </Form>
+    </Container>
   );
 }
 
