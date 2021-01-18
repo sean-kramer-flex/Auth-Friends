94fc2e7af5718145cd0de632a706fcc29c11981d
Sean Kramer <seandominickramer@gmail.com>
2021-01-18 16:04:05 -0500

    Add toggle function for editing friend

--- friends/src/Components/FriendItem.js
+++ friends/src/Components/FriendItem.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdEdit, MdDeleteForever } from 'react-icons/md'
 import { axiosWithAuth } from '../Utils/axiosWithAuth';
 
 function FriendItem(props) {
   const friends = props.friends
+
+const [isEdit, setIsEdit]=useState(false)
 
 const deleteFriend = (id) => {
   axiosWithAuth()
@@ -13,18 +15,40 @@ const deleteFriend = (id) => {
 }
 
   return (
-   <>
-        {friends.map(f => (
-          
+    isEdit === true ? (
+      <>
+      {friends.map(f => 
+        <tr  key={f.id}>
+        <td>
+          <input defaultValue ={f.name}/>
+        </td>
+        <td><input defaultValue={f.age} />
+        </td>
+        <td>
+          <input  defaultValue={f.email}/>
+        </td>
+        <td><i className="far fa-save" onClick={console.log('editted')}></i>
+        </td>
+        <td><i className="fas fa-trash"></i></td>
+      </tr>
+        )}
+      
+      </>
+    ) : (
+      <>
+      {friends.map(f => 
           <tr key={f.id}>
-            <td>{f.name}</td>
-            <td>{f.age}</td>
-            <td>{f.email}</td>
-            <td ><MdEdit /></td>
-            <td onClick={() => deleteFriend(f.id)}><MdDeleteForever /></td>
-          </tr>
-        ))}
-   </>
+          <td>{f.name}</td>
+          <td>{f.age}</td>
+          <td>{f.email}</td>
+          <td onClick={() => setIsEdit(true)}><MdEdit /></td>
+          <td onClick={() => deleteFriend(f.id)}><MdDeleteForever /></td>
+        </tr>
+        )
+        }
+ </>
+    )
+
   );
 }
 
